c1ddbd8665e7ef2006c47284aec059bf8c36448f
Rahul <rdhiman56@gmail.com>
2020-05-22 19:16:44 +0530

    database conneciton

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://cluster0-oaxs4.mongodb.net/test');
+mongoose.connection.once('open', () => {
+    console.log('connection has been made!');
+}).on('error', (error) => {
+    console.log('connection Error', error);
+})
 
 // Middlewear - bind express with graphql
 app.use('/graphql', graphqlHTTTP({
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,20 +1,58 @@
 const graphql = require('graphql');
 const _ = require('lodash');
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
+} = graphql;
 
 // dummy data
 var recipes = [
-    { name: 'Halwa', genre: 'demo1', id: '1' },
-    { name: 'Puri', genre: 'demo2', id: '2' },
-    { name: 'Chole', genre: 'demo3', id: '3' },
+    { name: 'Halwa', genre: 'demo1', id: '1', authorId: '1' },
+    { name: 'Puri', genre: 'demo2', id: '2', authorId: '2' },
+    { name: 'Chole', genre: 'demo3', id: '3',authorId: '3'},
+    { name: 'Broken Weat', genre: 'demo4', id: '3', authorId: '3' },
+    { name: 'custurd', genre: 'dmo4', id: '5', authorId: '3' },
+    { name: 'Milkshake', genre: 'demo5', id: '6', authorId: '3' },
+];
+
+var authors = [
+    { name: 'xyz1', age: 44, id: '1' },
+    { name: 'xyz2', age: 42, id: '2' },
+    { name: 'xyz3', age: 66, id: '3' }
 ];
 
 const RecipeType = new GraphQLObjectType({
     name: 'Recipe',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                console.log(parent);
+                return _.find(authors, { id: parent.authorId });
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        recipes: {
+            type: new GraphQLList(RecipeType),
+            resolve(parent, args){
+                return _.filter(recipes, { authorId: parent.id });
+            }
+        }
     })
 });
 
@@ -23,10 +61,30 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         recipe: {
             type: RecipeType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent,args){
                 // Code to get data from Database when request
+                console.log(typeof(args.id));
                 return _.find(recipes, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                return _.find(authors, { id: args.id });
+            }
+        },
+        recipes: {
+            type: GraphQLList(RecipeType),
+            resolve(parent, args){
+                return recipes;
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors;
             }
         }
     }
